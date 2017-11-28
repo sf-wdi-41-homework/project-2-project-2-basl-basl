82a8cd9a7db6a268bb77a808760e48143b81f77c
sgc88 <sehribancheng@yahoo.com>
2017-11-28 12:01:55 -0800

    user auth ready

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,18 @@
 class SessionsController < ApplicationController
 
-def index
-end
+
+    def create
+      user = User.find_by_email(params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to '/'
+      else
+        redirect_to '/sessions/new'
+      end
+    end
+
+    def destroy
+      session[:user_id] = nil
+      redirect_to '/sessions/new'
+    end
 end
