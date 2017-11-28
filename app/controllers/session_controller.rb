fdb1a8d0b1d51f072e3ec56fd5998f90123f2820
sgc88 <sehribancheng@yahoo.com>
2017-11-28 10:15:48 -0800

    minor change on controller session => sessions

--- app/controllers/session_controller.rb
+++
@@ -1,5 +0,0 @@
-class SessionController < ApplicationController
-
-def index
-end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'basil#index'
   resources :basil
   resources :user
-  resources :session
+  resources :sessions
 
 end
