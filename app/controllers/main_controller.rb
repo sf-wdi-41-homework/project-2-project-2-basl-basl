56d759be000ab6ef39be2ca551e19b2f043823c4
brwitten <bethrwitten@gmail.com>
2017-11-29 11:22:00 -0800

    API call is fixed using HTTParty

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,16 +1,17 @@
-require 'rest-client'
+require 'httparty'
 
 class MainController < ApplicationController
   def get_recipe
     puts params[:search]
     passed_ingredient = params[:search]
     puts passed_ingredient
-    response = RestClient.get "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=#{passed_ingredient}&limitLicense=false&number=5&ranking=1",
+    response = HTTParty.get "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=true&ingredients=#{passed_ingredient}&instructionsRequired=true&limitLicense=false&number=5&ranking=1",
     headers:{
       "X-Mashape-Key" => "OaoFJRKD3VmshnJHG7hkIiKIcObhp1DczU9jsnkF1spZ3TCI5E",
       "Accept" => "application/json"
     }
-    @response = response.body
+    @response = response
+    puts response
     render('results')
   end
 
