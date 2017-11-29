a5753319932c0040b8b96b377f595befdfa8182a
brwitten <bethrwitten@gmail.com>
2017-11-29 11:42:51 -0800

    API key is hidden

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,15 +1,19 @@
 require 'httparty'
 
 class MainController < ApplicationController
+  @@mashape_key = ENV["MASHAPEKEY"]
+
   def get_recipe
+    puts "KEY #{@@mashape_key}"
     puts params[:search]
     passed_ingredient = params[:search]
     puts passed_ingredient
     response = HTTParty.get "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=true&ingredients=#{passed_ingredient}&instructionsRequired=true&limitLicense=false&number=5&ranking=1",
     headers:{
-      "X-Mashape-Key" => "OaoFJRKD3VmshnJHG7hkIiKIcObhp1DczU9jsnkF1spZ3TCI5E",
+      "X-Mashape-Key" => @@mashape_key,
       "Accept" => "application/json"
     }
+    # puts ENV["MASHAPEKEY"]
     @response = response
     puts response
     render('results')
