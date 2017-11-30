032c88a2278e61ee9da65e4956670d5c185b8398
lperry34 <leviperry34@gmail.com>
2017-11-30 11:14:54 -0800

    formatted recipe's from api and added a logout for user auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
       user = User.find_by_email(params[:email])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        redirect_to '/'
+        redirect_to '/search'
       else
         redirect_to '/sessions/new'
       end
@@ -13,6 +13,6 @@ class SessionsController < ApplicationController
 
     def destroy
       session[:user_id] = nil
-      redirect_to '/sessions/new'
+      redirect_to '/'
     end
 end
