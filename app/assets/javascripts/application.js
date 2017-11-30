93099ea05292ddc69adb1faa51e7f8ba4616ccf2
lperry34 <leviperry34@gmail.com>
2017-11-30 09:15:49 -0800

    added confirmation for ingredients and upped the limit to 20 for responses

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
@@ -24,13 +13,15 @@ $(document).ready(function(){
             return this.value + ",";
         }).get().join(" ");
         firstValue = $('.searchbar').val(string);
+        $('.confirm').text("Your Ingredients:   " + $('.searchbar').val())
     }).trigger('change');
 
 
     $('.add').on('click', function(e){
       e.preventDefault();
       var single_item = $('.single').val()
-      $('.searchbar').val(firstValue.val() + single_item + ",");
+      var check = $('.searchbar').val(firstValue.val() + single_item + ",");
+      $('.confirm').text("Your Ingredients:   " + $('.searchbar').val());
 
   })
 });
