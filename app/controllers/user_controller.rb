50acf236a8f25c80648792f318c633d29884a421
lperry34 <leviperry34@gmail.com>
2017-11-30 11:14:54 -0800

    formatted recipe's from api and added a logout for user auth

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,9 +10,9 @@ class UserController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to '/search'
     else
-      redirect_to '/signup'
+      redirect_to '/user/new'
     end
   end
 
