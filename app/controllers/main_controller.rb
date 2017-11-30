65a72bcd30e3b8368a439075f348d3da3b8ea955
brwitten <bethrwitten@gmail.com>
2017-11-30 14:04:41 -0800

    austin's changes to main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,20 +3,28 @@ require 'httparty'
 class MainController < ApplicationController
   @@mashape_key = ENV["MASHAPEKEY"]
 
-  def get_recipe
-    puts "KEY #{@@mashape_key}"
-    puts params[:search]
+ def get_recipe
     passed_ingredient = params[:search]
-    puts passed_ingredient
-    response = HTTParty.get "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=true&ingredients=#{passed_ingredient}&instructionsRequired=true&limitLicense=false&number=8&ranking=1",
+    response = HTTParty.get "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=true&ingredients=#{passed_ingredient}&instructionsRequired=true&limitLicense=false&number=2&ranking=1",
     headers:{
       "X-Mashape-Key" => @@mashape_key,
       "Accept" => "application/json"
     }
     # puts ENV["MASHAPEKEY"]
     @response = response
-    puts response
-    render('results')
-  end
 
+
+     response.each do |recipe_id|
+        second_response = HTTParty.get "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/#{recipe_id["id"]}/information?includeNutrition=false",
+        headers:{
+          "X-Mashape-Key" => @@mashape_key,
+          "Accept" => "application/json"
+        }
+
+       @recipe = second_response
+        puts second_response.first
+      end
+
+   render('results')
+  end
 end
